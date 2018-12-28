b1a92e2792af7b6244f37c0b6c20576738c889d0
tla111 <tlamarketing111@gmail.com>
2018-12-28 14:29:08 -0500

    made chnages

---
+++ chart.js
@@ -0,0 +1,56 @@
+// let myChart = document.getElementById("myChart").getContext("2d");
+
+// let massPopChart = new myChart(myChart, {
+//     type: "bar",
+//     data: {
+//         labels: ["Washington DC", "Maryland", "Virginia"],
+//         datasets: [{
+//             label: "Population",
+//             data: [
+//                 100000,
+//                 200000,
+//                 300000
+//             ]
+//         }]
+//     },
+//     options: {}
+// });
+
+
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Washington DC", "Maryland", "Virginia", "Miami", "California", "New York"],
+        datasets: [{
+            label: '# of People',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
