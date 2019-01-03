a19fe7fbb576bc9c1e64c18e8f32a0267048489f
tla111 <tlamarketing111@gmail.com>
2019-01-02 21:59:26 -0500

    made changes

--- portfolio.js
+++ portfolio.js
@@ -3,7 +3,7 @@ onload  = start;
 function start(){	
 var i = 1;
 function Move(){ 	
-	i = (i%4)+1; // 4 is the Number of image in slider
+	i = (i%6)+1; // 4 is the Number of image in slider
 	document.getElementById('i'+i).checked = true;
 }
 setInterval(Move,1500); //change img in 1.5 sec
