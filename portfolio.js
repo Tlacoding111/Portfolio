158a5dac1b7401493260912a505f937426799765
tla111 <tlamarketing111@gmail.com>
2019-01-02 21:31:15 -0500

    made changes

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+onload  = start;
+
+function start(){	
+var i = 1;
+function Move(){ 	
+	i = (i%4)+1; // 4 is the Number of image in slider
+	document.getElementById('i'+i).checked = true;
+}
+setInterval(Move,1500); //change img in 1.5 sec
+}
