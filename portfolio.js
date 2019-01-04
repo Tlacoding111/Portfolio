58cc21cba5cf5c6d343d2ae63707f84345792bf0
tla111 <email@example.com>
2019-01-03 21:27:46 -0500

    made changes

--- portfolio.js
+++ portfolio.js
@@ -9,6 +9,7 @@ function Move(){
 setInterval(Move,3000); //change img in 3 sec
 }
 
+//Modal 1
 document.querySelector(".slideButton").addEventListener("click", () => 
 {
     document.querySelector(".bg-modal").style.display = "flex";
@@ -16,4 +17,24 @@ document.querySelector(".slideButton").addEventListener("click", () =>
 
 document.querySelector(".close").addEventListener("click", () => {
     document.querySelector(".bg-modal").style.display = "none";
+});
+
+//Modal 2
+document.querySelector(".secondModalButton").addEventListener("click", () => 
+{
+    document.querySelector(".bg-modal2").style.display = "flex";
+});
+
+document.querySelector(".closeTwo").addEventListener("click", () => {
+    document.querySelector(".bg-modal2").style.display = "none";
+});
+
+//Modal 3
+document.querySelector(".thirdModalButton").addEventListener("click", () => 
+{
+    document.querySelector(".bg-modal3").style.display = "flex";
+});
+
+document.querySelector(".closeThree").addEventListener("click", () => {
+    document.querySelector(".bg-modal3").style.display = "none";
 });
